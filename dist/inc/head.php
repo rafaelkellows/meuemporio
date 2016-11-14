7e7977a9d9c3a343ba95f16413fd544a8454d00c
Rafael Kellows <rafaelkellows@hotmail.com>
2016-11-14 12:13:59 -0200

    First Build

---
+++ dist/inc/head.php
@@ -0,0 +1,31 @@
+	<head>
+    	<title>Meu Empório - <?php print $page_title; ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+		<meta name="viewport" content="width=device-width,user-scalable=no">
+		<meta name="Keywords" content="Meu, Empório">
+		<meta name="Description" content="Meu Empório - Conheça uma solução mais eficiente e saudável para alimentação dos seus colaboradores">
+    
+	    <link rel="shortcut icon" href="images/cropped-logo-emporio-32x32.png">
+	    <link rel="icon" type="image/icon" href="images/cropped-logo-emporio-32x32.png">
+	    <link rel="apple-touch-icon" href="images/cropped-logo-emporio-32x32.png">
+
+	    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300" rel="stylesheet" type="text/css">
+	    <link rel="stylesheet" type="text/css" href="css/font-awesome.css" media="screen" />
+
+	    <!-- Owl Carousel Assets -->
+	    <link href="css/owl-carousel/owl.carousel.css" rel="stylesheet">
+	    <link href="css/owl-carousel/owl.theme.css" rel="stylesheet">
+
+	    <!-- build:css css/styles.min.css-->
+	    <link href="css/styles.css" rel="stylesheet">
+	    <!-- endbuild-->
+
+
+	    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+	    <script type='text/javascript' src="js/owl.carousel.min.js"></script>
+	    <script type='text/javascript' src='js/scripts.js'></script>
+
+		<!--[if lt IE 9]>
+		  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
